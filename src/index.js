3f0a2952b903e1a8c6129979c0bddf2109238437
pheelgit <philatov63@mail.ru>
2023-05-10 20:23:02 +0300

    remove react-query

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { QueryClient, QueryClientProvider } from 'react-query'
-
 import { ConfigProvider } from 'antd'
 import ru_RU from 'antd/locale/ru_RU'
 
@@ -11,15 +9,11 @@ import 'dayjs/locale/ru'
 import App from './app/App'
 import './index.css'
 
-const queryClient = new QueryClient()
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ConfigProvider locale={ru_RU}>
-        <App />
-      </ConfigProvider>
-    </QueryClientProvider>
+    <ConfigProvider locale={ru_RU}>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 )
